3705fe91a7e6324eef8351f7d9651a89e38587ea
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-08-12 10:23:50 +0200

    add multi-cloud

---
+++ stack-nexus/terraform/azure/main.tf
@@ -0,0 +1,57 @@
+module "nexus" {
+  #####################################
+  # Do not modify the following lines #
+  source   = "./module-nexus"
+  project  = var.project
+  env      = var.env
+  customer = var.customer
+  #####################################
+
+  #. extra_tags (optional): {}
+  #+ Dict of extra tags to add on aws resources. format { "foo" = "bar" }.
+  extra_tags = { "demo" = true }
+
+  # . keypair_public: ""
+  # + The public SSH key, for SSH access to newly-created instances
+  keypair_public = var.keypair_public
+
+  #
+  # VPC
+  #
+
+  #. cidr: 10.0.0.0/16
+  #+ The CIDR of the VPC
+  cidr = var.cidr
+
+  #. private_subnets (optional, list): ["10.0.1.0/24"]
+  #+ The private subnets for the VPC
+  private_subnets = var.private_subnets
+
+  #. public_subnets (optional, list): ["10.0.0.0/24"]
+  #+ The public subnets for the VPC
+  public_subnets = var.public_subnets
+
+  #
+  # Bastion
+  #
+
+  #. bastion_allowed_networks: 0.0.0.0/0
+  #+ Networks allowed to connect to the bastion using SSH
+  bastion_allowed_networks = var.bastion_allowed_networks
+
+  #. bastion_instance_type: t3.micro
+  #+ Instance type for the bastion
+  bastion_instance_type = var.bastion_instance_type
+
+  #
+  # Nexus Repository
+  #
+
+  #. nexus_instance_type: 't3.micro'
+  #+ Instance type for the Nexus Repository
+  nexus_instance_type = var.nexus_instance_type
+
+  #. nexus_disk_size: 20
+  #+ Disk size for the Nexus Repository (Go)
+  nexus_disk_size = var.nexus_disk_size
+}
---
+++ stack-nexus/terraform/azure/module-nexus/variables.tf
@@ -0,0 +1,77 @@
+# Cycloid
+variable "customer" {}
+variable "env" {}
+variable "project" {}
+
+# AWS
+variable "keypair_public" {
+  description = "The public SSH key, for SSH access to newly-created instances"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
+#
+# VPC
+#
+variable "cidr" {
+  type        = string
+  description = "The CIDR of the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "The private subnets for the VPC."
+  default     = ["10.0.1.0/24"]
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "The public subnets for the VPC."
+  default     = ["10.0.0.0/24"]
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+#
+# Bastion
+#
+variable "bastion_allowed_networks" {
+  description = "Networks allowed to connect to the bastion using SSH"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "bastion_instance_type" {
+  description = "Instance type for the bastion"
+  default     = "t3.micro"
+}
+
+
+#
+# Nexus Repository
+#
+variable "nexus_instance_type" {
+  description = "Instance type for the Nexus Repository"
+  default     = "t3.micro"
+}
+
+variable "nexus_disk_size" {
+  description = "Disk size for the Nexus Repository (Go)"
+  default = 20
+}
+
+# Tags
+variable "extra_tags" {
+  default = {}
+}
+
+locals {
+  standard_tags = {
+    "cycloid.io" = "true"
+    env          = var.env
+    project      = var.project
+    customer     = var.customer
+  }
+  merged_tags = merge(local.standard_tags, var.extra_tags)
+}
