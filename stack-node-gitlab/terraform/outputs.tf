bf2bb78908a53956e07f5633046c4963e4eaa934
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-09-17 18:09:45 +0200

    change name of tf module to app

--- stack-node-gitlab/terraform/outputs.tf
+++ stack-node-gitlab/terraform/outputs.tf
@@ -3,16 +3,16 @@
 #
 output "node_ip" {
   description = "The IP address the node server"
-  value       = module.node.node_ip
+  value       = module.app.node_ip
 }
 
 output "node_port" {
   description = "Port where node service is exposed"
-  value       = module.node.node_port
+  value       = module.app.node_port
 }
 
 output "node_os_user" {
   description = "The username to use to connect to the node instance via SSH."
-  value       = module.node.node_os_user
+  value       = module.app.node_os_user
 }
 
