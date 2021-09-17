952b283cab6be14ccf46b6af4bcff314c79c4c17
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-09-17 18:24:27 +0200

    change name of tf module to nodeapp

--- stack-node-gitlab/terraform/outputs.tf
+++ stack-node-gitlab/terraform/outputs.tf
@@ -3,16 +3,16 @@
 #
 output "node_ip" {
   description = "The IP address the node server"
-  value       = module.app.node_ip
+  value       = module.nodeapp.node_ip
 }
 
 output "node_port" {
   description = "Port where node service is exposed"
-  value       = module.app.node_port
+  value       = module.nodeapp.node_port
 }
 
 output "node_os_user" {
   description = "The username to use to connect to the node instance via SSH."
-  value       = module.app.node_os_user
+  value       = module.nodeapp.node_os_user
 }
 
